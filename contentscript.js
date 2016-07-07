8280536b2d6a3efd3de96c7db2d677be4544351c
Rob Wu <rob@robwu.nl>
2016-07-07 13:43:54 -0700

    4.3 Improve link fixing (e.g. Gmail and Maps)
    - Detect www.google.com in link even if current URL is not
      www.google.com (fixes detection in e.g. calender invites in Gmail).
    - Prevent .href from becoming an ugly URL (fixes e.g. link rewriting on
      contextmenu in Google Maps).

--- contentscript.js
+++ contentscript.js
@@ -1,5 +1,6 @@
 document.addEventListener('mousedown', handlePointerPress, true);
 document.addEventListener('touchstart', handlePointerPress, true);
+setupAggresiveUglyLinkPreventer();
 
 function handlePointerPress(e) {
     var a = e.target;
@@ -21,13 +22,76 @@ function handlePointerPress(e) {
         // not needed. It's not a problem to expose the Google origin to link
         // targets, since that is not private-sensitive information.
     }
-    if (a.hostname === location.hostname &&
+    var realLink = getRealLinkFromGoogleUrl(a);
+    if (realLink) {
+        a.href = realLink;
+    }
+}
+
+/**
+ * @returns {String} the real URL if the given link is a Google redirect URL.
+ */
+function getRealLinkFromGoogleUrl(a) {
+    if ((a.hostname === location.hostname || a.hostname === 'www.google.com') &&
         /^\/(local_)?url$/.test(a.pathname)) {
         // Google Maps / Dito (/local_url?q=<url>)
         // Mobile (/url?q=<url>)
-        var url = /[?&](?:q|url)=(http[^&]+)/.exec(a.search);
+        var url = /[?&](?:q|url)=(https?[%:][^&]+)/.exec(a.search);
         if (url) {
-            a.href = decodeURIComponent(url[1]);
+            return decodeURIComponent(url[1]);
         }
     }
+}
+
+/**
+ * Intercept the .href setter in the page so that the page can never change the
+ * URL to a tracking URL. Just intercepting mousedown/touchstart is not enough
+ * because e.g. on Google Maps, the page rewrites the URL in the contextmenu
+ * event at the bubbling event stage and then stops the event propagation. So
+ * there is no event-driven way to fix the URL. The DOMAttrModified event could
+ * be used, but the event is deprecated, so not a viable long-term solution.
+ */
+function setupAggresiveUglyLinkPreventer() {
+    // This content script runs as document_start, so we can have some assurance
+    // that the methods in the page are reliable.
+    var s = document.createElement('script');
+    s.textContent = '(' + function(getRealLinkFromGoogleUrl) {
+        var proto = HTMLAnchorElement.prototype;
+        // The link target can be changed in many ways, but let's only consider
+        // the .href attribute since it's probably the only used setter.
+        var hrefProp = Object.getOwnPropertyDescriptor(proto, 'href');
+        var hrefGet = Function.prototype.call.bind(hrefProp.get);
+        var hrefSet = Function.prototype.call.bind(hrefProp.set);
+        Object.defineProperty(proto, 'href', {
+            configurable: true,
+            enumerable: true,
+            get() {
+                return hrefGet(this);
+            },
+            set(v) {
+                hrefSet(this, v);
+                try {
+                    v = getRealLinkFromGoogleUrl(this);
+                    if (v) {
+                        hrefSet(this, v);
+                    }
+                } catch (e) {
+                    // Not expected to happen, but don't break the setter if for
+                    // some reason the (hostile) page broke the link APIs.
+                }
+            },
+        });
+        var setAttribute = Function.prototype.call.bind(proto.setAttribute);
+        proto.setAttribute = function(name, value) {
+            // Attribute names are not case-sensitive, but weird capitalizations
+            // are unlikely, so only check all-lowercase and all-uppercase.
+            if (name === 'href' || name === 'HREF') {
+                this.href = value;
+            } else {
+                setAttribute(this, name, value);
+            }
+        };
+    } + ')(' + getRealLinkFromGoogleUrl + ');';
+    (document.head || document.documentElement).appendChild(s);
+    s.remove();
 }
