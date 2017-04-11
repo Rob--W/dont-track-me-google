c2b85b050a930600694d363b97f60340c5bbb7df
Rob Wu <rob@robwu.nl>
2017-04-11 17:51:03 +0200

    Fix typo in user script gen, s/versiom/version/

--- make-userscript.js
+++ make-userscript.js
@@ -12,7 +12,7 @@ let metadata = [
     ['name', manifest.name],
     ['namespace', 'Rob W'],
     ['description', manifest.description],
-    ['versiom', manifest.version],
+    ['version', manifest.version],
     ['icon', 'https://raw.githubusercontent.com/Rob--W/dont-track-me-google/master/icon48.png'],
     ['run-at', content_script0.run_at.replace('_', '-')],
     ...content_script0.matches.map(pattern => ['match', pattern]),
